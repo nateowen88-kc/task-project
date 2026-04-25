148e25ceded0d5c2981ee505833348bf4e4cca17
nateowen88-kc <nateowen88@gmail.com>
2026-04-24 21:00:41 -0500

    Merge admin routing into auth function

--- api/admin/[...path].ts
+++
@@ -1,3 +0,0 @@
-import adminHandler from "../../src/server/native/admin-handler.js";
-
-export default adminHandler;
--- api/auth/[...path].ts
+++ api/auth/[...path].ts
@@ -1,3 +1,14 @@
+import adminHandler from "../../src/server/native/admin-handler.js";
 import authHandler from "../../src/server/native/auth-handler.js";
+import { getPathname } from "../../src/server/native/http.js";
 
-export default authHandler;
+export default async function authEntry(request: Parameters<typeof authHandler>[0], response: Parameters<typeof authHandler>[1]) {
+  const pathname = getPathname(request);
+
+  if (pathname.startsWith("/api/admin")) {
+    await adminHandler(request, response);
+    return;
+  }
+
+  await authHandler(request, response);
+}
